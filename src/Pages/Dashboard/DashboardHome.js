d098728e86b467e626ce85a0948f64f60ba54762
shnooki <vikisahputra17@gmail.com>
2023-02-13 20:42:15 +0700

    place redirect success register to login

---
+++ src/Pages/Dashboard/DashboardHome.js
@@ -0,0 +1,12 @@
+import Dashboard from "../../Components/User/Dashboard"
+import Navbar from "../../Components/Navbar"
+const DashboardHome = () => {
+    return (
+    <>
+        <Navbar/>
+        <Dashboard/>
+    </>
+    )
+}
+ 
+export default DashboardHome
